6cf18281fab254051da482b659c6f58089f7655c
Allison Ause <allison.ause@gmail.com>
2022-10-11 17:58:46 -0700

    added drop down menu using three different approaches to distilling the types into one single, discreet array

--- src/components/Spells/Spells.js
+++ src/components/Spells/Spells.js
@@ -4,11 +4,50 @@ import './Spells.css';
 
 export default function Spells(id) {
   const { spells, loading } = useSpells(id);
-  if (loading) return <span className="loader"></span>;
+  console.log('spells', spells);
+
+  // ForEach Logic APPROACH:
+  // let typeArray = [];
+  // spells.forEach((spell) => {
+  //   if (!typeArray.includes(spell.type)) return typeArray.push(spell.type);
+  // });
+
+  // REDUCER APPROACH
+  const typeArray = spells.reduce(
+    (acc, curr) => (acc.includes(curr.type) ? acc : [...acc, curr.type]),
+    []
+  );
+  // SET APPROACH
+  // const spellsArr = spells.map((spell) => spell.type);
+  // const typeArray = [...new Set(spellsArr)];
+  console.log('typeArray', typeArray);
+
+  if (loading) return <span className="loader-div"></span>;
 
   return (
     <div className="home">
       <h2>Hello There!</h2>
+      <div>
+        <h3>Find Just the Right Spell:</h3>
+        {/* <select>
+          Spell Type:
+          <option value="Any">Any</option>
+          {spells.map((spell) => (
+            <option key={spell.id} value={spell.type}>
+              {spell.type}
+            </option>
+          ))}
+        </select> */}
+        <select>
+          Spell Type:
+          <option value="Any">Any</option>
+          {typeArray.map((type) => (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className="spells-container">
         <h3>Spells List:</h3>
         <SpellsList spells={spells} />
