cd27660880054705b865dda1117bca41d9f38d16
Allison Ause <allison.ause@gmail.com>
2023-09-05 16:36:25 -0700

    updated package.json

--- src/components/SpellCard/SpellCard.js
+++ src/components/SpellCard/SpellCard.js
@@ -5,7 +5,10 @@ export default function SpellCard({ name, incantation, effect }) {
   //change the name to uppercase for each word
   //put in conditional for if incantation !=== null, render h3.
   return (
-    <div className="spell-card" style={{ backgroundImage: `url(${background})` }}>
+    <div
+      className="spell-card"
+      style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover' }}
+    >
       <h2>{incantation}</h2>
       <h3>{name}</h3>
       <span>{effect}</span>
